b2dc631210c4bf96a42745bab4ea65d0a99f3e6b
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-12 15:43:12 -0300

    Chore: Add Image to model

---
+++ shop-backend/src/main/java/com/majesty/shopbackend/model/Image.java
@@ -0,0 +1,26 @@
+package com.majesty.shopbackend.model;
+
+import java.sql.Blob;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
