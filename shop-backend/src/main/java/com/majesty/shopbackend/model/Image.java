5c108977ee605fbe4eb7522c7d5da43ba785dd26
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-12 16:27:21 -0300

    Fix: Add entity to files

--- shop-backend/src/main/java/com/majesty/shopbackend/model/Image.java
+++ shop-backend/src/main/java/com/majesty/shopbackend/model/Image.java
@@ -2,13 +2,21 @@ package com.majesty.shopbackend.model;
 
 import java.sql.Blob;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- shop-backend/src/main/java/com/majesty/shopbackend/model/Product.java
+++ shop-backend/src/main/java/com/majesty/shopbackend/model/Product.java
@@ -4,13 +4,21 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
